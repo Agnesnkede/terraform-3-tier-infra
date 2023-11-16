685cbd17f188a15bde6fe5fc5d0ed22d51e5c330
agnes nkede <abwembole@gmail.com>
2023-11-15 20:28:54 -0500

    added ec2 module

---
+++ vpc/variables.tf
@@ -0,0 +1,48 @@
+variable "vpc_cidr_block" {
+  description = "CIDR block for the main VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet1_cidr_block" {
+  description = "CIDR block for the public subnet 1"
+  type        = string
+  default     = "10.0.1.0/24"
+}
+
+variable "public_subnet2_cidr_block" {
+  description = "CIDR block for the public subnet 2"
+  type        = string
+  default     = "10.0.2.0/24"
+}
+
+variable "private_subnet1_cidr_block" {
+  description = "CIDR block for the private subnet 1"
+  type        = string
+  default     = "10.0.3.0/24"
+}
+
+variable "private_subnet2_cidr_block" {
+  description = "CIDR block for the private subnet 2"
+  type        = string
+  default     = "10.0.4.0/24"
+}
+
+variable "availability_zones" {
+  type = list(string)
+  description = "(optional) describe your variable"
+  default = [ "us-east-1a","us-east-1b" ]
+  
+} 
+  variable "tags" {
+    type = map
+    description = "tag"
+    
+  }
+
+  variable "vpc_id" {
+    type = string
+    description = "VPC CIDR range"
+    default = "10.0.0.0/16"
+}
+  
